30286b7ed76369cdc4a5b779050fa26b402dcc8c
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-10 00:40:07 +0100

    :truck: app use router animes added

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
+const animesRouter = require("./routers/animesRouter");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
+
+app.use("/animes", animesRouter);
 
 app.use(generalError);
 app.use(notFoundError);
