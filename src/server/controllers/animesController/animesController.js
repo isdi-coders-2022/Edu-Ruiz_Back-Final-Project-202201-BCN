e789c04e41e21c7b35bc660fd3b7b66346125c35
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-12 13:15:28 +0100

    :sparkles: controller anime deleted and tested 100%

--- src/server/controllers/animesController/animesController.js
+++ src/server/controllers/animesController/animesController.js
@@ -5,4 +5,15 @@ const getAllAnimes = async (req, res) => {
   res.json({ animes });
 };
 
-module.exports = { getAllAnimes };
+const deleteAnime = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Anime.findByIdAndDelete(id);
+    res.status(200).json(deleted);
+  } catch (error) {
+    error.message = "cant delete anime";
+    next(error);
+  }
+};
+
+module.exports = { getAllAnimes, deleteAnime };
--- src/server/controllers/animesController/animesController.test.js
+++ src/server/controllers/animesController/animesController.test.js
@@ -1,7 +1,11 @@
 const Anime = require("../../../database/models/Anime");
-const { getAllAnimes } = require("./animesController");
+const { getAllAnimes, deleteAnime } = require("./animesController");
 
 jest.mock("../../../database/models/Anime");
+
+const mockAnimeDelete = jest.spyOn(Anime, "findByIdAndDelete");
+const mockAnimeGet = jest.spyOn(Anime, "find");
+
 describe("Given an getAllAnimes controller", () => {
   beforeEach(() => {
     jest.resetAllMocks();
@@ -22,6 +26,41 @@ describe("Given an getAllAnimes controller", () => {
       await getAllAnimes(null, res);
       expect(Anime.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith({ animes });
+    });
+  });
+});
+
+describe("Given a deleteAnime controller", () => {
+  describe("When it receives a response with status 200 and object with id 3", () => {
+    test("Then it should delete the review with id 3", async () => {
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const id = "3";
+      const req = {};
+      req.params = { id };
+      const status = 200;
+      const expectedDeletedReview = { id: "3" };
+      const next = jest.fn();
+      mockAnimeDelete.mockResolvedValue(expectedDeletedReview);
+
+      await deleteAnime(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+      expect(res.json).toHaveBeenCalledWith({ id });
+    });
+  });
+
+  describe("When it receives a response", () => {
+    test("Then it should call function next with message 'cant delete animes'", async () => {
+      const error = new Error();
+      mockAnimeGet.mockImplementation(() => Promise.reject(error));
+      const expectedErrorMessage = "cant delete anime";
+      const expectedError = new Error(expectedErrorMessage);
+
+      const next = jest.fn();
+
+      await deleteAnime(null, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
 });
