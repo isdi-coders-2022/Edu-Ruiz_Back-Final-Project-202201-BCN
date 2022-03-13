0e0bf8c2ca6aad13b8622c7dfca0250c2f006a78
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-03-13 03:36:05 +0100

    :zap: createAnime controller

--- src/server/controllers/animesController/animesController.js
+++ src/server/controllers/animesController/animesController.js
@@ -1,4 +1,75 @@
+const debug = require("debug")("anime4me: animeController");
+const { initializeApp } = require("firebase/app");
+const {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} = require("firebase/storage");
+const path = require("path");
+const fs = require("fs");
+
+const chalk = require("chalk");
 const Anime = require("../../../database/models/Anime");
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const fireBaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(fireBaseApp);
+
+const createAnime = async (req, res, next) => {
+  const { name, autor } = req.body;
+  try {
+    const nameAnimeExists = await Anime.findOne({ name });
+    if (nameAnimeExists) {
+      const error = new Error(`Anime already exists!`);
+      error.code = 400;
+      next(error);
+      return;
+    }
+    const oldFileName = path.join("public", req.file.filename);
+    const newFileName = path.join("public", req.file.originalname);
+    fs.rename(oldFileName, newFileName, (error) => {
+      if (error) {
+        next(error);
+      }
+    });
+    fs.readFile(newFileName, async (error, file) => {
+      if (error) {
+        next(error);
+      } else {
+        const storageRef = ref(
+          storage,
+          `${Date.now()}_${req.file.originalname}`
+        );
+        await uploadBytes(storageRef, file);
+        const firebaseFileURL = await getDownloadURL(storageRef);
+        const newAnime = await Anime.create({
+          name,
+          autor,
+          image: firebaseFileURL,
+        });
+        debug(chalk.cyanBright(`Anime created with name: ${newAnime.name}`));
+        res.status(201);
+        res.json({
+          message: `Anime created with name: ${newAnime.name}`,
+        });
+      }
+    });
+  } catch (error) {
+    fs.unlink(path.join("uploads", req.file.filename), () => {
+      error.code = 400;
+      next(error);
+    });
+  }
+};
 
 const getAllAnimes = async (req, res) => {
   const animes = await Anime.find();
@@ -16,4 +87,4 @@ const deleteAnime = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllAnimes, deleteAnime };
+module.exports = { getAllAnimes, deleteAnime, createAnime };
